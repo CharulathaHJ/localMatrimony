d27f24411f19da55c7b8121fb680c66b5335a2e9
CharulathaHJ <charumanu98@gmail.com>
2021-08-23 00:51:43 +0530

    modified index.js

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-
-import postRoutes from './routes/posts';
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('./database');
+const cors = require('cors');
+const path = require('path');
 const app = express();
-
+const postRoutes = require('./routes/posts');
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-
 app.use('/posts', postRoutes);
-
 const CONNECTION_URL = 'mongodb+srv://pichar:pichar0902@cluster0.vuha9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 app.use(express.static(path.join(__dirname, '../build')))
