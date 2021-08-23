30b10373b83f626439d814bb185917ac5cea9da7
CharulathaHJ <charumanu98@gmail.com>
2021-08-23 21:02:32 +0530

    removed extra port call

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(cors());
 app.use('/posts', postRoutes);
 const CONNECTION_URL = 'mongodb+srv://pichar:pichar0902@cluster0.vuha9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
+    res.sendFile(path.join(__dirname, './client/build'))
 })
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
